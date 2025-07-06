deb1fe37da4eebd757c881460fbc7229fab08450
Thomas Ottosson <tomasottosson1@gmail.com>
2025-07-06 02:30:04 +0200

    added urls and libaries to project folder and app folder in the urls.py files

--- fitness/urls.py
+++ fitness/urls.py
@@ -13,3 +13,72 @@ urlpatterns = [
         views.nutrition_plan_list,
         name='nutrition_plan_list'
     ),
+    path('products/', views.product_list, name='product_list'),
+    path(
+        'exercise-plans/<int:pk>/',
+        views.exercise_plan_detail,
+        name='exercise_plan_detail'
+    ),
+    path(
+        'nutrition-plans/<int:pk>/',
+        views.nutrition_plan_detail,
+        name='nutrition_plan_detail'
+    ),
+    path(
+        'products/<int:pk>/',
+        views.product_detail,
+        name='product_detail'
+    ),
+    path(
+        'add-to-cart/<str:item_type>/<int:pk>/',
+        views.add_to_cart,
+        name='add_to_cart'
+    ),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path(
+        'cart/update/<int:item_id>/',
+        views.update_cart_item,
+        name='update_cart_item'
+    ),
+    path(
+        'cart/remove/<int:item_id>/',
+        views.remove_from_cart,
+        name='remove_from_cart'
+    ),
+    path('checkout/', views.checkout, name='checkout'),
+    path('payment-success/', views.payment_success, name='payment_success'),
+    path('accounts/orders/', views.order_history, name='order_history'),
+    path(
+        'subscribe/<str:item_type>/<int:pk>/',
+        views.create_subscription_checkout,
+        name='create_subscription_checkout'
+    ),
+    path('register/', views.register, name='register'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path(
+        'dashboard/',
+        views.subscribed_dashboard,
+        name='subscribed_dashboard'
+    ),
+    path(
+        'exercise-plans/<int:pk>/content/',
+        views.exercise_plan_content,
+        name='exercise_plan_content'
+    ),
+    path(
+        'nutrition-plans/<int:pk>/content/',
+        views.nutrition_plan_content,
+        name='nutrition_plan_content'
+    ),
+    path(
+        'newsletter/signup/',
+        views.newsletter_signup,
+        name='newsletter_signup'
+    ),
+    path('staff-dashboard/', views.staff_dashboard, name='staff_dashboard'),
+    path(
+        'order-confirmation/',
+        views.order_confirmation,
+        name='order_confirmation'
+    ),
+]
--- fitness_community_by_t/urls.py
+++ fitness_community_by_t/urls.py
@@ -1,22 +1,58 @@
-"""
-URL configuration for fitness_community_by_t project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.contrib.sitemaps.views import sitemap
+from fitness.sitemaps import (
+    StaticViewSitemap,
+    ExercisePlanSitemap,
+    NutritionPlanSitemap,
+    ProductSitemap
+)
+from fitness.views import custom_404_view
+from django.contrib.auth.views import LoginView
+from fitness.forms import CustomAuthenticationForm
+
+#SITEMAP CONFIGURATION
+sitemaps = {
+    'static': StaticViewSitemap,
+    'exercise_plans': ExercisePlanSitemap,
+    'nutrition_plans': NutritionPlanSitemap,
+    'products': ProductSitemap,
+}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('fitness.urls')),
+    path(
+        'accounts/login/',
+        LoginView.as_view(
+            template_name='registration/login.html',
+            authentication_form=CustomAuthenticationForm
+        ),
+        name='login'
+    ),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'robots.txt',
+        serve,
+        {'path': 'robots.txt', 'document_root': settings.STATICFILES_DIRS[0]}
+    ),
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+# handler404 = custom_404_view
+handler404 = 'fitness.views.custom_404_view'
