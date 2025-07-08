48c1f25ab756aecde51c3941eeccce6aa1e8c556
Thomas Ottosson <tomasottosson1@gmail.com>
2025-07-09 00:08:55 +0200

    added templatetags folder with init file and rating_extras

---
+++ fitness/templatetags/rating_extras.py
@@ -0,0 +1,11 @@
+from decimal import Decimal, InvalidOperation
+from django import template
+
+register = template.Library()
+
+@register.filter
+def round_int(value):
+    try:
+        return int(round(Decimal(value)))
+    except (TypeError, InvalidOperation):
+        return 0
